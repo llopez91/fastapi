d4c339056a19db88d218c5d5cd6d2e39d36d5186
Luis Enrique López Hernández <l.lopez@traxion.global>
2022-04-08 16:44:13 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'blog pusblished list {limit}'}
+    else:
+        return {'data': f'blog list {limit}'}
+
+
+@app.get('/blog/unpupished')
+def unpublished():
+    return "unpublished"
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'Blog is created with {request.title}'}
